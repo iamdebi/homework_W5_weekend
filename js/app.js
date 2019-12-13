cfe53ae761aba352c04391d21aece794d80e77bf
Debbie <debiurquhart@live.co.uk>
2019-12-13 18:52:15 +0000

    adds layout styling

--- js/app.js
+++ js/app.js
@@ -7,24 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     console.dir(formElement);
     const formList = document.querySelector("#band-list");
+    const formListFav = document.querySelector("#band-list-fav");
     const newListItem = document.createElement("li");
 
-    let allTime;
     const bandName = this.name.value;
     const bandLead = this.lead.value;
     const bandGenre = this.genre.value;
     const favourite = this.favourite.value;
 
     if (favourite === "true") {
-      allTime = "it was true";
+      newListItem.textContent = `${bandName} ${bandLead} ${bandGenre}`;
+      formListFav.appendChild(newListItem);
     } else {
-      allTime = "it was not true";
+      newListItem.textContent = `${bandName} ${bandLead} ${bandGenre}`;
+      formList.appendChild(newListItem);
     }
 
-    newListItem.textContent = `${bandName} ${bandLead} ${bandGenre} ${allTime}`;
-    formList.appendChild(newListItem);
     formElement.reset();
   };
+
   formElement.addEventListener("submit", handleFormSubmit);
 
   const handleDelete = function() {
